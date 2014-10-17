524f4232b9a2cc7574d3931699ee03696dd1d294
franciscofacebook <franciscofacebook@hotmail.es>
2014-10-17 00:45:02 -0600

    desde CERO

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/SistemaFpi';
+$config['base_url']	= 'http://localhost/ZukistrukizFPI';
 
 /*
 |--------------------------------------------------------------------------
