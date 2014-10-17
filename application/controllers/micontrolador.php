c7f872bce6ea5adf1f0ab62553ada1a78b48a567
franciscofacebook <franciscofacebook@hotmail.es>
2014-10-17 00:54:37 -0600

    Ya con bootstrap y vista y controlador

---
+++ application/controllers/micontrolador.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Micontrolador	 extends CI_Controller {
+		
+		function __construct(){
+			
+			parent::__construct();
+ 
+		$this->load->helper('form');
+							}
+		public function index ()
+		{
+			$this->load->helpers('url'); 
+			$this->load->view('vista');
+		}
+		
+	
+}
+
+
+?>
+	
---
+++ application/views/vista.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<link  rel="stylesheet" href="<?php  echo base_url(); ?>/css/bootstrap.css"/>
+	
+</head>
+<body>	
+	<div id="container">
+	<h1> Fran :D </h1>
+	<p>Hola</p>
+	<button class="btn btn-warning"> Aqui</button>
+	</div> 
+</body>
+</html>
