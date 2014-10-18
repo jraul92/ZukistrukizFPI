22de20f175aa78b02919cbbad495ff6810939f6e
g3rson <gc19682@gmail.com>
2014-10-18 03:00:41 -0600

    yo

--- application/helpers/footer_helper.php
+++ application/helpers/footer_helper.php
@@ -3,10 +3,14 @@
 function FOO()
 {
      $foo = '<footer>
+     			<img src="<?php echo base_url()/imgs/fb.jpg?>">
      			<a>Like/Facebook</a>
+     			<img src="<?php echo base_url()/imgs/tw.jpg?>">
      			<a>Siguenos en twitter</a>
+     			<img src="<?php echo base_url()/imgs/em.jpg?>">
      			<a>Contactanos al correo</a>
-     			<asing>DErechos Reservados SIDECONINV R</asing>
+     			<b> <br>
+     			<asing>*Derechos Reservados SIDECONINV R</asing>
      
      		</footer>
     ';
