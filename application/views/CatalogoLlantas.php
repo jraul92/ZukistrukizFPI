3caea9bb60094f7095270c032a86cc0f806a2874
DCFigueroa <dc.figueroa17@gmail.com>
2014-10-18 03:43:43 -0600

    catalogollantas

---
+++ application/views/CatalogoLlantas.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		
+	</head>
+	
+	<body>
+		<?php echo NAV();?>
+		<div id="categoriacatalogo">
+			<h4>Categorias</h4>
+			<li>
+				<ol>Marca</ol>
+				<ol>Modelo</ol>
+				<ol>Rin</ol>
+			</li>
+			<br />
+			<h4>Precios</h4>
+		</div>
+		<div></div>
+	</body>
+	
+	<footer><?php echo FOO();?></footer>	
+</html>
