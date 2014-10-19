4ef47adba2327682b1615dd266a8d879b20f41dd
g3rson <gc19682@gmail.com>
2014-10-18 19:49:45 -0600

    yo

--- application/helpers/footer_helper.php
+++ application/helpers/footer_helper.php
@@ -3,11 +3,11 @@
 function FOO()
 {
      $foo = '<footer>
-     			<img src="<?php echo base_url()/imgs/fb.jpg?>">
+     			<img src="<?php echo base_url()/imgs/fb.jpg..?>">
      			<a>Like/Facebook</a>
-     			<img src="<?php echo base_url()/imgs/tw.jpg?>">
+     			<img src="<?php echo base_url()/imgs/tw.jpg..?>">
      			<a>Siguenos en twitter</a>
-     			<img src="<?php echo base_url()/imgs/em.jpg?>">
+     			<img src="<?php echo base_url()/imgs/em.jpg..?>">
      			<a>Contactanos al correo</a>
      			<b> <br>
      			<asing>*Derechos Reservados SIDECONINV R</asing>
--- application/views/vista.php
+++ application/views/vista.php
@@ -9,9 +9,16 @@
 <body>	
 	<?php echo NAV();?>
 	<div id="container">
-	<h1> :D </h1>
-	<p>:p</p>
-	<h1> la del pirata cojo con pata de palo :3 </h1>
+	<h1>lorem ipsum</h1>
+	<p>Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, 
+		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+		Excepteur sint occaecat cupidatat non proident, 
+		sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<h1>  </h1>
 	<button class="btn btn-warning"> Aqui</button>
 	</div> 
 	
