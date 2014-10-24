44707370cc98c26e189555cdb949f8b20d4032d1
DCFigueroa <dc.figueroa17@gmail.com>
2014-10-24 03:34:54 -0600

    Cambios Deisy

--- application/views/CatalogoLlantas.php
+++ application/views/CatalogoLlantas.php
@@ -4,10 +4,11 @@
 		<meta charset="utf-8">
 		<title>Catalogo Llantas</title>
 		<link  rel="stylesheet" href="<?php  echo base_url(); ?>/css/bootstrap.css"/>
+		<link  rel="stylesheet" href="<?php  echo base_url(); ?>/css/css.css"/>
 	</head>
 	<body>
 		<?php echo NAV();?>
-		<div id="categoriacatalogo">
+		<div class="categoriacatalogo">
 			<h4>Categorias</h4>
 			<ul>
 				<li><a>Marca</a></li>
@@ -23,7 +24,14 @@
 			</ul>
 			<br />
 		</div>
-		<div></div>
+		<div class="catalogo">
+			<p> hifie </p>
+			<h3>kajcfhke</h3>
+			<br />
+			<br />
+			<br />
+			<br />
+		</div>
 	</body>
 	
 	<footer><?php echo FOO();?></footer>	
