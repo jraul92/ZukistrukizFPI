ac73b390cf5d521e6d011bc3ec824b82d5b32b01
DCFigueroa <dc.figueroa17@gmail.com>
2014-10-23 23:59:58 -0600

    Cambios Deisy

--- application/views/CatalogoLlantas.php
+++ application/views/CatalogoLlantas.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		
-	</head>
 		<meta charset="utf-8">
 		<title>Catalogo Llantas</title>
 		<link  rel="stylesheet" href="<?php  echo base_url(); ?>/css/bootstrap.css"/>
+	</head>
 	<body>
 		<?php echo NAV();?>
 		<div id="categoriacatalogo">
 			<h4>Categorias</h4>
 			<ul>
-				<li>Marca</li>
-				<li>Modelo</li>
-				<li>Rin</li>
+				<li><a>Marca</a></li>
+				<li><a>Modelo</a></li>
+				<li><a>Rin</a></li>
 			</ul>
 			<br />
 			<h4>Precios</h4>
 			<ul>
-				<li>$20-$30</li>
-				<li>$30-$40</li>
-				<li>$40-$50</li>
+				<li><a>$20 - $30</a></li>
+				<li><a>$30 - $40</a></li>
+				<li><a>$40 - $50</a></li>
 			</ul>
 			<br />
 		</div>
