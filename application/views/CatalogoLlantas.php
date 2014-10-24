a2a69da0ff225629be5cc61139a6bb523d3e11ee
DCFigueroa <dc.figueroa17@gmail.com>
2014-10-23 23:00:55 -0600

    Cambio de Deisy

--- application/views/CatalogoLlantas.php
+++ application/views/CatalogoLlantas.php
@@ -3,22 +3,24 @@
 	<head>
 		
 	</head>
-	
+		<meta charset="utf-8">
+		<title>Welcome to CodeIgniter</title>
+		<link  rel="stylesheet" href="<?php  echo base_url(); ?>/css/bootstrap.css"/>
 	<body>
 		<?php echo NAV();?>
 		<div id="categoriacatalogo">
 			<h4>Categorias</h4>
 			<li>
-				<ol>Marca</ol>
-				<ol>Modelo</ol>
-				<ol>Rin</ol>
+				<ul>Marca</ul>
+				<ul>Modelo</ul>
+				<ul>Rin</ul>
 			</li>
 			<br />
 			<h4>Precios</h4>
 			<li>
-				<ol>$20-$30</ol>
-				<ol>$30-$40</ol>
-				<ol>$40-$50</ol>
+				<ul>$20-$30</ul>
+				<ul>$30-$40</ul>
+				<ul>$40-$50</ul>
 			</li>
 			<br />
 		</div>
